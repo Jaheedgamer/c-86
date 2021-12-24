009410f2ed660df979e116afe0a85552636ca54e
Jaheedgamer <88264622+Jaheedgamer@users.noreply.github.com>
2021-12-24 09:40:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+
+canvas.add(BirthdayImage.jpg)
+
+function playSound(){
+    document.getElementById("myAudio").play()
+}
